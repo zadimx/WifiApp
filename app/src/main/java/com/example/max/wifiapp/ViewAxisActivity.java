5e2a4e8c89f05fa46fc58014a11ffe83d0bde8d6
Max <zamaxzamax@gmail.com>
2019-08-20 10:23:16 +0300

    First final app

--- app/src/main/java/com/example/max/wifiapp/ViewAxisActivity.java
+++ app/src/main/java/com/example/max/wifiapp/ViewAxisActivity.java
@@ -253,6 +253,7 @@ public class ViewAxisActivity extends AppCompatActivity {
         }
     };
 
+//
     public static boolean hasConnection(final Context context)
     {
         ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
